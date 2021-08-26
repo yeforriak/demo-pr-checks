4c56f3a5c5c4acf76fdffb7144ebf57fdd4fa285
yeforriak <yeforriak@gmail.com>
2021-08-26 18:00:03 +0200

    feat: add new toMillis endpoint

--- src/service.js
+++ src/service.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const ms = require("ms");
 const port = 3000;
+
+const SECRET = "secret-token-xxxxxxxxx";
 
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/toMillis", (req, res) => {
+  const period = req.query.period;
+  const millis = ms(period);
+  res.send(`${period} = ${millis} millis`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
